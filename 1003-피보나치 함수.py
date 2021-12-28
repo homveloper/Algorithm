b6d3a5ff28b35776666d6e6a9be04eb82fef6a1e
ToTo-Mo <bum4496@naver.com>
2021-12-28 20:00:39 +0900

    update 피보나치 함수

--- 1003-피보나치 함수.py
+++ 1003-피보나치 함수.py
@@ -1,22 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+inputs = sys.stdin.readlines
+
+T = int(input())
+L = list(map(int, inputs()))
+
+DP = [[0,0] for i in range(41)]
+
 def fibonacci(n):
-    global zero, one
 
     if n == 0:
-        zero += 1
-        return 0
+        return [1,0]
     elif n == 1:
-        one += 1
-        return 1
-    
-    return fibonacci(n -1) + fibonacci(n - 2)
+        return [0,1]
 
-if __name__ == "__main__":
-    T = int(input())
+    if DP[n][0] or DP[n][1]:
+        return DP[n]
+    else:
+        A = fibonacci(n -1)
+        B = fibonacci(n - 2) 
+        DP[n][0] = A[0] + B[0]
+        DP[n][1] = A[1] + B[1]
+        return DP[n]
 
-    for _ in range(T):
-        zero = 0
-        one = 0
-
-        fibonacci(int(input()))
-
-        print(zero, one)
+for i in L:
+   print(*fibonacci(i))
