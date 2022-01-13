c7c1ff03070a96b78a3c88ee7d12c79ed4f80a88
ToTo-Mo <bum4496@naver.com>
2022-01-13 16:32:28 +0900

    update 숨바꼭질

---
+++ 1697-숨바꼭질.py
@@ -0,0 +1,33 @@
+from sys import stdin
+from collections import deque
+input = stdin.readline
+
+N, K = map(int, input().split())
+INF = 200000
+
+def bfs(start, end) -> int:
+    queue = deque([start])
+    visited_time = [INF]*100001
+    visited_time[start] = 0
+
+    if start == end:
+        return 0
+
+    while queue:
+        p = queue.popleft()
+        if 0 <= p-1 <= 100000 and visited_time[p-1] == INF:
+            visited_time[p-1] = visited_time[p]+1
+            queue.append(p-1)
+
+        if 0 <= p+1 <= 100000 and visited_time[p+1] == INF:
+            visited_time[p+1] = visited_time[p]+1
+            queue.append(p+1)
+
+        if 0 < p*2<= 100000 and visited_time[p*2] == INF:
+            visited_time[p*2] = visited_time[p]+1
+            queue.append(p*2)
+
+    return visited_time[end]
+
+print(bfs(N,K))
+# print(bfs(K,N))
