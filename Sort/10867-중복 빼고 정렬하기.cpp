d0d09305e0da243d4e2fbe43faa1c2289055d168
homveloper <bum4496@naver.com>
2022-03-03 10:28:17 +0900

    추가 : 10867 중복 빼고 정렬하기

---
+++ Sort/10867-중복 빼고 정렬하기.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+const int IDX_OFFSET = 1000;
+const int MAX = 2001;
+
+int N;                  // 수의 개수
+int L[MAX] = {0};      // 1번이라도 나온 결과라면 1로 저장 -1000 부터 0번째 인덱스
+
+void Input()
+{
+    cin>>N;
+}
+
+void Solution()
+{
+    for(int i=0; i<N; i++){
+        int n;  cin>>n;
+
+        L[n+IDX_OFFSET] = 1;
+    }
+
+    for(int i=0; i<MAX; i++){
+        if(L[i]) cout<<i-IDX_OFFSET<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Input();
+    Solution();
+}
