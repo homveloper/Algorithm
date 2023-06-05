6dfe3b47fe383badaccbae94f6d4ea18754ad081
homveloper <bum4496@naver.com>
2023-06-05 22:31:41 +0900

    [추가] 11021 A+B-7 rust

---
+++ Loop/11021-A+B-7/main.rs
@@ -0,0 +1,21 @@
+fn solution(a : u8, b : u8) -> u8 {
+    a + b
+}
+
+fn main() {
+    let mut test_case = String::new();
+    std::io::stdin().read_line(&mut test_case).unwrap();
+
+    let test_case : i32 = test_case.trim().parse().unwrap();
+
+    for i in 0..test_case {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        let mut iter = input.trim().split_whitespace();
+        let a : u8 = iter.next().unwrap().parse().unwrap();
+        let b : u8 = iter.next().unwrap().parse().unwrap();
+
+        println!("Case #{}: {}", i+1,solution(a,b));
+    }
+}   
